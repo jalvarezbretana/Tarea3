0a6e4ec379f22094536deaf6542d8c9ba901339a
jalvarezbretana <jalvarezbretana@danielcastelao.org>
2020-05-09 16:14:11 +0200

    Tarefa 3 rematada

---
+++ src/tarea3/Tarea3.java
@@ -0,0 +1,19 @@
+package tarea3;
+
+/**
+ *
+ * @author josel
+ */
+public class Tarea3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Ventana vent = new Ventana();
+    vent.setVisible(true);
+    
+    
+    }
+    
+}
